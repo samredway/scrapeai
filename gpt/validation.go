e61c12f75a89248ea3601ff6fd3c76e88bb4cccd
Sam Redway <sam.redway@outlook.com>
2024-11-21 05:05:16 +0000

    check nested objects for additionalProperties

--- gpt/validation.go
+++ gpt/validation.go
@@ -5,22 +5,65 @@ import (
 	"fmt"
 )
 
-// Validate the response schema that we provide to GPT for return
-// data
+// Validate the response schema that we provide to GPT for return data
 func ValidateSchema(schema string) error {
-	// check that the schema is valid json
 	var obj any
 	err := json.Unmarshal([]byte(schema), &obj)
 	if err != nil {
 		return fmt.Errorf("schema is not valid json: %w", err)
 	}
-	// TODO this must be iterative and find all nested obj
-	var outer = obj.(map[string]any)
-	if outer["type"] == "object" {
-		value, exists := outer["additionalProperties"]
-		if !exists || value != false {
-			return fmt.Errorf("An object must contain the additionalProperties field and it must be false")
+	return checkByType(obj.(map[string]any))
+}
+
+func checkByType(obj map[string]any) error {
+	value, exists := obj["type"]
+	if !exists {
+		return checkNested(obj)
+	}
+	switch value {
+	case "object":
+		return checkObj(obj)
+	case "array":
+		return checkArr(obj)
+	case "string":
+		// TODO
+		return nil
+	default:
+		return fmt.Errorf("Invalid value for key 'type' must be 'object' 'array' or 'string'")
+	}
+}
+
+func checkNested(obj map[string]any) error {
+	excludedKeys := map[string]bool{
+		"additionalProperties": true,
+		"type":                 true,
+		"required":             true,
+	}
+	for key, value := range obj {
+		if excludedKeys[key] {
+			continue
+		}
+		if nestedMap, ok := value.(map[string]any); ok {
+			return checkByType(nestedMap)
+		} else {
+			return fmt.Errorf("Expected value for key %s to be map[string]any, but got %T", key, value)
 		}
 	}
 	return nil
+}
+
+func checkObj(obj map[string]any) error {
+	value, exists := obj["additionalProperties"]
+	if !exists || value != false {
+		return fmt.Errorf("An object must contain the additionalProperties field and it must be false")
+	}
+	return checkNested(obj)
+}
+
+func checkArr(obj map[string]any) error {
+	_, exists := obj["items"]
+	if !exists {
+		return fmt.Errorf("An array object must contain the key 'items'")
+	}
+	return checkNested(obj)
 }
